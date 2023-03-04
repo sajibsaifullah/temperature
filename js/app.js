2866b8c4825ee722698df3136a28b5f578d9a2ec
sajibsaifullah <dusajibphy@gmail.com>
2023-03-04 10:39:41 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// do not show API key on your JS file
+const API_KEY = `4a493174be8f94db702c64b1c91090f2`;
+const loadTemperature = city => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+
+const displayTemperature = data => {
+    // const temperature = document.getElementById('temperature');
+    // temperature.innerText = data.main.temp;
+    setInnerTextById('temperature', data.main.temp);
+    setInnerTextById('condition', data.weather[0].main);
+
+    // console.log(data.weather[0].main);
+}
+
+const setInnerTextById = (id, text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+}
+
+document.getElementById('btn-search').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    // set city
+    document.getElementById('city').innerText = city;
+    loadTemperature(city);
+})
+
+loadTemperature('dhaka');
